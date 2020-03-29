f1004befe5cc442a94f27cc33f9437b49bc27df8
jdfrozen <616769416@qq.com>
2020-03-29 20:54:56 +0800

    init frozen-file-yun

---
+++ src/main/java/com/frozen/frozenfileyun/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.frozen.frozenfileyun.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Auther: frozen
+ * @Date: 2020/3/29 20:40
+ * @Description:
+ */
+@Controller
+public class IndexController {
+//    @RequestMapping(value = "index")
+//    public String index() {
+//        return "index";
+//    }
+}
